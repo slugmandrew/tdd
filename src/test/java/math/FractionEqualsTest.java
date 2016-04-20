1663189a49d666f09a681ca3117a8e28fc2bd089
Drew Spencer <slugmandrew@gmail.com>
2016-04-20 12:23:34 +0100

    Equals works, except for reducing

--- src/test/java/math/FractionEqualsTest.java
+++ src/test/java/math/FractionEqualsTest.java
@@ -3,6 +3,7 @@ package math;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * @author Drew Spencer
@@ -13,5 +14,29 @@ public class FractionEqualsTest
     public void sameNumeratorAndDenominator() throws Exception
     {
         assertEquals(new Fraction(3, 5), new Fraction(3, 5));
+    }
+
+    @Test
+    public void differentNumerators() throws Exception
+    {
+        assertNotEquals(new Fraction(1, 5), new Fraction(2, 5));
+    }
+
+    @Test
+    public void differentDenominators() throws Exception
+    {
+        assertNotEquals(new Fraction(3, 4), new Fraction(3, 7));
+    }
+
+    @Test
+    public void wholeNumbersEqualsSameFraction() throws Exception
+    {
+        assertEquals(new Fraction(5, 1), new Fraction(5));
+    }
+
+    @Test
+    public void wholeNumberNotEqualToWholeNumber() throws Exception
+    {
+        assertNotEquals(new Fraction(6), new Fraction(65));
     }
 }
